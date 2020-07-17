7bb6f7d43e118445d063e572b7adc76eed63be59
Karan Boro <68140398+slothtae@users.noreply.github.com>
2020-07-17 13:57:49 +0530

    Add files via upload

---
+++ Day 3/even or odd/eo.js
@@ -0,0 +1,11 @@
+console.log("This is the Even odd program");
+
+var num=prompt(`Input a number :`);
+
+if( num%2 ==0){
+    console.log(`The numbe entered id ${num} ans the Number is even.`);
+}
+else{
+    console.log(`The numbe entered id ${num} ans the Number is odd.`);
+}
+
