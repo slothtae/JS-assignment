a26c92113b14d4638c3a55c247454b3231c2befc
Karan Boro <68140398+slothtae@users.noreply.github.com>
2020-07-20 15:31:54 +0530

    day 4 assignments

---
+++ Day 4/Q2/da.js
@@ -0,0 +1,13 @@
+console.log(`The Console.`);
+
+const student = {
+    name: `helsinki`,
+    age: 24,
+    projects:{
+        diceGame:"Two player dice game using JavaScript"
+    }
+}
+
+const{name,age,projects:{diceGame}} = student;
+
+console.log(name,age,diceGame);
---
+++ Day 4/Q3/array.js
@@ -0,0 +1,7 @@
+console.log(`The Console.`);
+
+let shoppingList=[`vegies`,`cereals`,`fruits`];
+console.log(shoppingList);
+let shoppingBasket = shoppingList.slice();
+shoppingBasket.push(`soap`,`detergent`,`chocolate`)
+console.log(shoppingBasket);
