50c63035770b19391fd86b9f2f98a1ded845e043
Karan Boro <68140398+slothtae@users.noreply.github.com>
2020-07-20 16:57:20 +0530

    Add files via upload

---
+++ Day 4/Q6/g.js
@@ -0,0 +1,10 @@
+console.log(`The Console`);
+
+let n =prompt(`Input number greater than 100:`);
+
+while(n<=100){
+    n=prompt(`Input number greater than 100:`);
+}
+if(n>100){
+    console.log(`${n} is greater than 100.`)
+}
